5df7b224f3e5ef90d192723f32793a4945b7c9c0
gogochiou <aaaa8552369@gmail.com>
2021-12-21 15:19:31 +0800

    add include

---
+++ include/Manager.h
@@ -0,0 +1,15 @@
+#ifndef _MANAGER_H_
+#define _MANAGER_H_
+
+#include <iostream>
+#include <string>
+
+class Manager{
+private:
+    /* data */
+public:
+    Manager(/* args */);
+    ~Manager();
+};
+
+#endif //_MANAGER_H_
